fc8dc1c045354ae6afd78545293f3dc5e5ef5c6c
Sai Abhilash Abhay Wadegaonkar <saiabhilash.wadegaonkar@publicissapient.com>
2020-12-22 21:39:06 +0530

    Updates

---
+++ precache-manifest.e8f9e40773d062731ea4d6e75a1fc828.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd86b4495fd8feaf98c7fb2ed9283c4",
+    "url": "/rickandmorty-app/index.html"
+  },
+  {
+    "revision": "818be40258af218db90a",
+    "url": "/rickandmorty-app/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "b3af4a5b934165051b12",
+    "url": "/rickandmorty-app/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "818be40258af218db90a",
+    "url": "/rickandmorty-app/static/js/2.8a6a02f3.chunk.js"
+  },
+  {
+    "revision": "25724449fa7b7df3da3572d055e0b556",
+    "url": "/rickandmorty-app/static/js/2.8a6a02f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3af4a5b934165051b12",
+    "url": "/rickandmorty-app/static/js/main.cc84a6ff.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/rickandmorty-app/static/js/main.cc84a6ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c65a0a428132c2b60e",
+    "url": "/rickandmorty-app/static/js/runtime-main.b148b478.js"
+  },
+  {
+    "revision": "e10e0b738020fd50a53a7133834bce73",
+    "url": "/rickandmorty-app/static/media/app.e10e0b73.jpg"
+  },
+  {
+    "revision": "700ea584cc400f1846250967af0ad909",
+    "url": "/rickandmorty-app/static/media/logo7.700ea584.png"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/rickandmorty-app/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "eddf5d2d370528340d263e68eb77b134",
+    "url": "/rickandmorty-app/static/media/one.eddf5d2d.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/rickandmorty-app/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/rickandmorty-app/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/rickandmorty-app/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/rickandmorty-app/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "c37086a479611b3c084c9f9a1ff3a8a0",
+    "url": "/rickandmorty-app/static/media/two.c37086a4.jpg"
+  }
+]);
